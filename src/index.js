bde9d033bf4f8fe206dc0587b321b1ba888139a4
luizsmatos <gusstavopdm@gmail.com>
2022-01-10 10:00:07 -0300

    feature: É possível criar uma conta

--- src/index.js
+++ src/index.js
@@ -1,4 +1,38 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { customers } = require('../models/Customers');
 const app = express();
+
+app.use(express.json());
+
+/**
+ * ID - UUID
+ * CPF - String
+ * Nome - String
+ * Extratos - Array
+ */
+
+app.post('/account', (req, res) => {
+  const { cpf, name } = req.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return res.status(400).json({
+      error: 'Customer already exists!',
+    });
+  }
+
+  customers.push({
+    id: uuidv4(),
+    cpf,
+    name,
+    statement: [],
+  });
+
+  return res.status(201).json({ message: 'Customer created'});
+});
 
 app.listen(3333);
