fdbc52a09b321178533af4462d153bd7eb25e5d2
luizsmatos <gusstavopdm@gmail.com>
2022-01-10 12:14:45 -0300

    feature: É possível atualizar os dados de um cliente

--- src/index.js
+++ src/index.js
@@ -108,4 +108,20 @@ app.get('/statement/date', verifyCustomerExists, (req, res) => {
   return res.json(statement);
 });
 
+app.put('/account', verifyCustomerExists, (req, res) => {
+  const { name } = req.body;
+  const { customer } = req;
+
+  customer.name = name;
+
+  return res.status(201).json({ message: 'Customer updated' });
+})
+
+app.get('/account', verifyCustomerExists, (req, res) => {
+  const { customer } = req;
+
+  return res.json(customer);
+})
+
+
 app.listen(3333);
